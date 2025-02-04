6c443a3e6258123268cb53992dbf2469c124ef51
Peter van der Heijden <peter@192.168.2.15>
2025-02-05 00:01:16 +0100

    added shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { SiDiscord, SiGithub, SiLinkedin } from '@icons-pack/svelte-simple-icons';
+</script>
+
+<main class="flex h-screen flex-col items-center justify-center space-y-1">
+	<h1
+		class="bg-gradient-to-r from-[#0077FF] to-[#FF00FB] bg-clip-text text-4xl font-bold uppercase italic text-transparent md:text-5xl"
+	>
+		Peter Jarian
+	</h1>
+	<h2 class="md:text-xl">IT-student and Full-stack developer</h2>
+
+	<div class="flex flex-row space-x-4">
+		<SiGithub size={32} />
+		<SiLinkedin />
+		<SiDiscord />
+	</div>
+</main>
