ab9356def55fca9095b548b490515dfaeac7d11c
Peter van der Heijden <contact@peterjarian.dev>
2025-10-16 17:30:07 +0200

    removed app version for now

--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,12 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import z from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import z from 'zod';
 
 export const env = createEnv({
   client: {},
   server: {
-    NOTION_TOKEN: z.string(),
     APP_VERSION: z.string(),
   },
   runtimeEnv: {
-    NOTION_TOKEN: process.env.NOTION_TOKEN,
     APP_VERSION: process.env.APP_VERSION,
   },
 });
